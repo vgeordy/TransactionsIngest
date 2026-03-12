2fb699ea785f69f24ec5fd2f2b20a09a547ff39f
vgeordy <57164841+vgeordy@users.noreply.github.com>
2026-03-12 14:27:21 -0400

    add Program.cs, appsettings.json, and project configuration

---
+++ TransactionsIngest.App/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        var connectionString = context.Configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlite(connectionString));
+        services.AddScoped<AuditService>();
+        services.AddScoped<TransactionService>();
+        services.AddScoped<ITransactionApiClient, MockTransactionApiClient>();
+
+    })
+    .Build();
+
+var transactionService = host.Services.GetRequiredService<TransactionService>();
+await transactionService.ProcessTransactionsAsync();
